fbb8929d655be09eb8c96130b309bb58baa0b4c5
jorgenico0105 <jorgengc1234@gmail.com>
2025-07-21 17:20:31 -0500

    Learning go

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+type Persona struct {
+	Age       uint
+	Height    float64
+	Weight    float64
+	SkinColor string
+	Familia   struct {
+		numeroHijos int
+		padreNombre string
+	}
+	mascotas []Animal
+}
+type Animal struct {
+	Especies string
+	Sonido   string
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+	//javier := Persona{}
+	nicolas := Persona{
+		Age:       30,
+		Height:    1.75,
+		Weight:    70.5,
+		SkinColor: "Moreno",
+		mascotas:  []Animal{{Especies: "Perro", Sonido: "Guau"}, {Especies: "Gato", Sonido: "Miau"}},
+	}
+	fmt.Println("Edad:", nicolas.Age)
+	nicolas.Age = 31
+	fmt.Println("Nueva Edad:", nicolas.Age)
+	sumAge(&nicolas)
+	fmt.Println("Edad despues de la funcion:", nicolas.Age)
+	nombre := "Nicolas"
+	nombre2 := "Gonzalez"
+	_, apellido := returnNames(nombre, nombre2)
+	fmt.Println("El apellido es:", apellido)
+}
+func returnNames(nombre1, nombre2 string) (string, string) {
+	return nombre1, nombre2
+}
+func sumAge(persona *Persona) {
+	persona.Age += 20
+}
