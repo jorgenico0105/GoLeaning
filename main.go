67f238969c2f0235ff1e2aea51d8962cb91f4064
jorgenico0105 <jorgengc1234@gmail.com>
2025-07-22 14:54:14 -0500

    Learning go part - 1

--- main.go
+++ main.go
@@ -1,6 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+type Transporte interface {
+	sonido() string
+	consumo() string
+}
+type Carro struct {
+	modelo string
+	motor  uint
+}
+type Moto struct {
+	modelo string
+	motor  uint
+}
+type Barco struct {
+	modelo string
+	motor  uint
+}
+
+func (c Carro) sonido() string {
+	return "Vroom Vroom"
+}
+func (c Carro) consumo() string {
+	return "10 L/100km"
+}
+func (m Moto) sonido() string {
+	return "Brrr Brrr"
+}
+func (m Moto) consumo() string {
+	return "5 L/100km"
+}
+func (b Barco) sonido() string {
+	return "Splash Splash"
+}
+func calcularConsumo(t Transporte) {
+	fmt.Println(t.consumo())
+}
+func imprimirSonido(t Transporte) {
+	fmt.Println(t.sonido())
+}
 
 type Persona struct {
 	Age       uint
@@ -51,6 +93,10 @@ func main() {
 		password, username := u.getUsername()
 		fmt.Println("Usuario:", i, "Username:", username, "Password:", password)
 	}
+	var carro Transporte = Carro{modelo: "Toyota", motor: 2000}
+	var moto Transporte = Moto{modelo: "Yamaha", motor: 150}
+	imprimirSonido(carro)
+	imprimirSonido(moto)
 	fmt.Println("Edad:", nicolas.Age)
 	nicolas.Age = 31
 	fmt.Println("Nueva Edad:", nicolas.Age)
@@ -60,10 +106,44 @@ func main() {
 	nombre2 := "Gonzalez"
 	_, apellido := returnNames(nombre, nombre2)
 	fmt.Println("El apellido es:", apellido)
+	i, err := strconv.Atoi("145")
+	if err != nil {
+		fmt.Println("Error converting string to int:", err.Error())
+		return
+	}
+	fmt.Println("Converted integer:", i)
+	us, err := getUserById(3)
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		return
+	}
+	// while en go
+	//cont := 0
+	//for cont < 5{
+	//  fmt.Println("Contador:", cont)
+	// cont++
+	//}
+	fmt.Println("User found:", us)
 }
 func returnNames(nombre1, nombre2 string) (string, string) {
 	return nombre1, nombre2
 }
 func sumAge(persona *Persona) {
 	persona.Age += 20
+}
+
+type UserId struct {
+	id   uint
+	name string
+}
+
+func getUserById(id uint) (string, error) {
+	users := []UserId{{id: 1, name: "Nicolas"}, {id: 2, name: "Javier"}}
+	for _, user := range users {
+		if user.id == id {
+			return user.name, nil
+		}
+
+	}
+	return "", fmt.Errorf("User with id %d not found", id)
 }
