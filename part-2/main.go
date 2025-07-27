d5359c3a9baa5b49e2733626d2488be2f8283797
Jorge Nicolas Granda Costa <jorgengc1234@gmail.com>
2025-07-27 13:00:20 -0400

    Learning go slices and mappers

--- part-2/main.go
+++ part-2/main.go
@@ -15,16 +15,19 @@ func main() {
 	//Fomra de declarar un map o construit
 	fmt.Println("Usuarios:", usuarios)
 	for _, user := range usuarios {
-		_, existe := mapper[user.id]
-		if !existe {
+		if _, existe := mapper[user.id]; !existe {
 			mapper[user.id] = user
 		}
-
 		u := mapper[user.id]
 		u.roles = append(u.roles, user.rol)
 		mapper[user.id] = u
 	}
 	fmt.Println("Usuarios:", mapper)
+	for k, v := range mapper {
+		fmt.Println("ID:", k, "Usuario:", v)
+	}
+
+	fmt.Println("Usuarios con roles:")
 	fmt.Println("This is Class2")
 	numeros := []int{7, 8, 9, 10, 11, 12, 13}
 	masNumeros := []int{14, 15, 16, 17, 18, 19, 20}
@@ -40,6 +43,8 @@ func main() {
 	fmt.Println("Despues de agregar nombres:", nombres)
 	numeros = append(numeros, masNumeros...)
 	fmt.Println("Despues de agregar mas numeros:", numeros)
+	_, number2 := testFunction(5, 8)
+	fmt.Println("Number2:", number2)
 }
 
 func agregar(numeros []int) {
@@ -48,4 +53,9 @@ func agregar(numeros []int) {
 }
 func agregarNames(names []string) {
 	names = append(names, "Roger")
+}
+func testFunction(number, number2 int) (sum, sum2 int) {
+	sum = number + number2
+	sum2 = (number * 3) + number2
+	return number, number2
 }
