379131e7cfacc019a7c30770e8117dd82e1585d0
Jorge Nicolas Granda Costa <jorgengc1234@gmail.com>
2025-08-02 14:18:40 -0400

    Pointers

--- advanced/main.go
+++ advanced/main.go
@@ -2,7 +2,28 @@ package main
 
 import "fmt"
 
+type comida interface {
+	tiempoPreparacion() string
+}
+type pizza struct {
+	peso   int
+	precio float64
+}
+
+func (p pizza) tiempoPreparacion() string {
+	return "Tiempo de preparacion de la pizza: 30 minutos"
+}
+func imprimirTiempo(c comida) {
+	fmt.Println(c.tiempoPreparacion())
+}
+
+type Persona struct {
+	name string
+	age  int
+}
+
 func main() {
+	Nicolas := Persona{name: "Nicolas", age: 30}
 	fmt.Println("Hello, Advanced Go!")
 	suma := sumar(5, 3)
 	fmt.Println("The sum is:", suma)
@@ -14,8 +35,21 @@ func main() {
 	phrase, count := fullPhrase("from Go!")
 	fmt.Println(phrase)
 	fmt.Println("Word count:", count)
+	fmt.Println("Persona:", Nicolas)
+	//AddFiveYears(&Nicolas)
+	Nicolas.AddFiveYears()
+	fmt.Println("Name after sum ages: ", Nicolas)
+	x := 2
+	y := &x
+	fmt.Println("Value of x:", x)
+	fmt.Println("Address of x:", &x)
+	fmt.Println("Value of y (address of x):", y)
+	fmt.Println("Value at address y (value of x):", *y)
 }
 
+func (p *Persona) AddFiveYears() {
+	p.age += 5
+}
 func sumar(a, b int) int {
 	defer restar(a, b) // This will execute after sumar
 	return a + b
